dbd222e09816754eae2c37c89f19fae6e0bb0001
lucasilvas2 <lucasoliver1068@gmail.com>
2022-02-16 18:08:51 -0300

    Ajuste ordem chamadas das funçoes

--- script/ibge.js
+++ script/ibge.js
@@ -82,18 +82,20 @@ function retornaCodeAlpha2ComparandoONomeAbreviado(nomeAbreviado){
     });
     return codeAlpha2Encontrado;
 }
-
+var controle = 1;
 $(".radio-option").change(function(){
     var opcaoEscolhida = document.getElementsByName('opcao_escolhida');
     var inputDisplaySegundoPais = document.getElementById('escolhaPais2');
     
     if (opcaoEscolhida[0].checked == true) {        
         inputDisplaySegundoPais.style.display = 'none';
-        cachePageOpcaoEscolhida = localStorage.setItem('opcaoEscolhida', ("#"+document.getElementsByName('opcao_escolhida')[0].value), 'mais um valor');     
+        cachePageOpcaoEscolhida = localStorage.setItem('opcaoEscolhida', (document.getElementsByName('opcao_escolhida')[0].value), 'mais um valor');
+        controle = 1;     
     }
     else{
         inputDisplaySegundoPais.style.display = 'inline'
-        cachePageOpcaoEscolhida= localStorage.setItem('opcaoEscolhida', "#"+document.getElementsByName('opcao_escolhida')[1].value);
+        cachePageOpcaoEscolhida= localStorage.setItem('opcaoEscolhida', document.getElementsByName('opcao_escolhida')[1].value);
+        controle = 2; 
     }
     
 }
@@ -108,18 +110,16 @@ $(".entradaPais").change(function(){
     if((opcao_escolhida[0].checked == true && localStorage.getItem('pais1escolhido') != paisEscolhido1.value) || (opcao_escolhida[1].checked == true && paisEscolhido1.value != paisEscolhido2.value)){
         buscarDados();
     }
-    
 });
 
-function carramendoLocalStorage(){
+function carregamentoLocalStorage(){
     
     var opcao_escolhida = document.getElementsByName('opcao_escolhida');
     var paisEscolhido1 = document.getElementById('escolhaPais1');
     var paisEscolhido2 = document.getElementById('escolhaPais2'); 
     
     if(localStorage.length == 3){
-        printConsole(localStorage.length)
-        if(localStorage.getItem('opcaoEscolhida') == '#opcao2'){           
+        if(localStorage.getItem('opcaoEscolhida') == 'opcao2'){           
             //opcao_escolhida[1].checked ='true';
             $( "#opcao2" ).trigger( "click" );
             //paisEscolhido1.value = ;
@@ -128,14 +128,14 @@ function carramendoLocalStorage(){
             $(localStorage.getItem('pais2escolhido')).trigger( "click" );
             //paisEscolhido2.value = localStorage.getItem('pais2escolhido');
         }
-        else if(localStorage.getItem('opcaoEscolhida') == '#opcao1'){
+        else if(localStorage.getItem('opcaoEscolhida') == 'opcao1'){
             //paisEscolhido1.value = localStorage.getItem('pais1escolhido');
             $(localStorage.getItem('pais1escolhido')).trigger( "click" );
         }
     }
 }
 var cachePageOpcaoEscolhida;
-var controle;
+
 var paisEscolhido1;
 var paisEscolhido2
 var codePais1;
@@ -146,31 +146,30 @@ function buscarDados(){
     opcaoEscolhida = document.getElementsByName('opcao_escolhida');
     paisEscolhido1 = document.getElementById('escolhaPais1');
     paisEscolhido2 = document.getElementById('escolhaPais2');
-    cachePais1 = localStorage.setItem('pais1escolhido', '#'+paisEscolhido1.value);
-    cachePais2 = localStorage.setItem('pais2escolhido', '#'+paisEscolhido2.value);
+    cachePais1 = localStorage.setItem('pais1escolhido', paisEscolhido1.value);
+    cachePais2 = localStorage.setItem('pais2escolhido', paisEscolhido2.value);
+    verificaresultado();
     if (opcaoEscolhida[0].checked == true) {
         codePais1 = paisEscolhido1.value;
-        controle = 1;
-        xhttpAssincrono(dadosGrafico, 4, codePais1);
-        xhttpAssincrono(mostrarInformacaoPais, 2, codePais1);    
-          
+        xhttpAssincrono(mostrarInformacaoPais, 2, codePais1); 
+        xhttpAssincrono(dadosGrafico, 4, codePais1);                      
     }
     else if (opcaoEscolhida[1].checked == true){
         
         codePais1 = paisEscolhido1.value;
         codePais2 = paisEscolhido2.value;
-        controle = 2;
+        xhttpAssincrono(mostrarInformacaoPais, 3, codePais1, codePais2);   
         xhttpAssincrono(dadosGrafico, 5, codePais1, codePais2);
-        xhttpAssincrono(mostrarInformacaoPais, 3, codePais1, codePais2);        
+             
     }
     
-    alertaEscolha(controle, paisEscolhido1.value, paisEscolhido2.value);
+    
 }
 
 var infoPais
 function mostrarInformacaoPais(value){
     infoPais = JSON.parse(value);
-    verificaresultado();
+    //verificaresultado();
     
     var resultado = document.getElementById('resultado');
     
@@ -181,7 +180,8 @@ function mostrarInformacaoPais(value){
 
         resultado.insertAdjacentHTML('beforeend', '<div id="graficoInvestimentosemPesquisaeDesenvolvimento" class="grafico container-fluid justify-content-center" > País não possui dados </div>');
 
-        resultado.insertAdjacentHTML('beforeend', '<div id="graficoGastosComEducacao" class="grafico container-fluid justify-content-center" > País não possui dados </div>');       
+        resultado.insertAdjacentHTML('beforeend', '<div id="graficoGastosComEducacao" class="grafico container-fluid justify-content-center" > País não possui dados </div>');
+              
     }
     else if(controle == 2){
         resultado.insertAdjacentHTML('beforeend', `<div class="d-flex justify-content-center" id="info_pais">  </div>`);
@@ -197,25 +197,22 @@ function mostrarInformacaoPais(value){
         resultado.insertAdjacentHTML('beforeend', '<div id="graficoInvestimentosemPesquisaeDesenvolvimento" class="grafico container-fluid justify-content-center" > País não possui dados </div>');
 
         resultado.insertAdjacentHTML('beforeend', '<div id="graficoGastosComEducacao" class="grafico container-fluid justify-content-center" > País não possui dados </div>');  
+       
     }
-  
-    google.charts.setOnLoadCallback(drawVisualizationIPD);
-    google.charts.setOnLoadCallback(drawVisualizationGastosComEducacao);
-    google.charts.setOnLoadCallback(drawVisualizationPIB);
     
-  
+    // google.charts.setOnLoadCallback(drawVisualizationIPD);
+    // google.charts.setOnLoadCallback(drawVisualizationGastosComEducacao);
+    // google.charts.setOnLoadCallback(drawVisualizationPIB);
+   
 }
 
-
 function verificaresultado(){
-    var x = document.getElementById('resultado').children.length;
-    //printConsole(x);
-    if(x > 0 ){
-        document.getElementById("resultado").remove()
-        //printConsole('deletei');
-        var main = document.getElementById('main');
-        main.insertAdjacentHTML('beforeend', '<div class="resultado h-auto" id="resultado"></div>')   
-    }
+    var x = document.getElementById('resultado').childElementCount;
+    printConsole('delete');
+    document.getElementById("resultado").remove()
+    //printConsole('deletei');
+    var main = document.getElementById('main');
+    main.insertAdjacentHTML('beforeend', '<div class="resultado h-auto justify-content-center" id="resultado"></div>')   
 }
 
 function alertaEscolha(controle, pais1, pais2){
@@ -269,10 +266,7 @@ var paisGrafico1;
 var paisGrafico2;
 function dadosGrafico(value){
     
-    dadosJSON = JSON.parse(value);
-    dadosListaGastosComEducacao = [];
-    dadosListaIPD  = [];
-    dadosListaPIB  = [];
+    dadosJSON = JSON.parse(value);    
     if(controle == 1){
         paisGrafico1 = paisEscolhido1.value;
         if(dadosJSON[0].series.length != 0 ){
@@ -284,9 +278,15 @@ function dadosGrafico(value){
             dadosListaGastosComEducacao[5] = [tratandoDado(dadosJSON[0].series[0].serie[48][2020])];
         }else{
             
-            for(let i = 0; i < 6; i++){           
-                dadosListaGastosComEducacao[i] = [0];                         
-            }
+            // for(let i = 0; i < 6; i++){           
+            //     dadosListaGastosComEducacao[i] = [0];                         
+            // }
+            dadosListaGastosComEducacao[0] = [0];
+            dadosListaGastosComEducacao[1] = [0];
+            dadosListaGastosComEducacao[2] = [0];
+            dadosListaGastosComEducacao[3] = [0];
+            dadosListaGastosComEducacao[4] = [0];
+            dadosListaGastosComEducacao[5] = [0];
         }
 
         if(dadosJSON[1].series.length != 0 ){
@@ -297,9 +297,15 @@ function dadosGrafico(value){
             dadosListaIPD[4] = [tratandoDado(dadosJSON[1].series[0].serie[39][2015])];
             dadosListaIPD[5] = [tratandoDado(dadosJSON[1].series[0].serie[48][2020])];
         }else{
-            for(let i = 0; i < 6; i++){
-                dadosListaIPD[i] = [0];
-            }
+            // for(let i = 0; i < 6; i++){
+            //     dadosListaIPD[i] = [0];
+            // }
+            dadosListaIPD[0] = [0];
+            dadosListaIPD[1] = [0];
+            dadosListaIPD[2] = [0];
+            dadosListaIPD[3] = [0];
+            dadosListaIPD[4] = [0];
+            dadosListaIPD[5] = [0];
         }
 
         if(dadosJSON[2].series.length != 0 ){
@@ -310,9 +316,15 @@ function dadosGrafico(value){
             dadosListaPIB[4] = [tratandoDado(dadosJSON[2].series[0].serie[39][2015])];
             dadosListaPIB[5] = [tratandoDado(dadosJSON[2].series[0].serie[48][2020])];
         }else{
-            for(let i = 0; i < 6; i++){
-                dadosListaPIB[i] = [0];
-            }
+            // for(let i = 0; i < 6; i++){
+            //     dadosListaPIB[i] = [0];
+            // }
+            dadosListaPIB[0] = [0];
+            dadosListaPIB[1] = [0];
+            dadosListaPIB[2] = [0];
+            dadosListaPIB[3] = [0];
+            dadosListaPIB[4] = [0];
+            dadosListaPIB[5] = [0];
         }
     }
     else if(controle == 2){
@@ -332,8 +344,7 @@ function dadosGrafico(value){
                 dadosListaGastosComEducacao[3] = [0,tratandoDado(dadosJSON[0].series[1].serie[28][2010])];
                 dadosListaGastosComEducacao[4] = [0,tratandoDado(dadosJSON[0].series[1].serie[39][2015])];
                 dadosListaGastosComEducacao[5] = [0,tratandoDado(dadosJSON[0].series[1].serie[48][2020])];
-            }else{
-                printConsole("aqui");
+            }else if(dadosJSON[0].series[1] == null){
                 dadosListaGastosComEducacao[0] = [tratandoDado(dadosJSON[0].series[1].serie[3][1995]), 0];
                 dadosListaGastosComEducacao[1] = [tratandoDado(dadosJSON[0].series[1].serie[6][2000]), 0];
                 dadosListaGastosComEducacao[2] = [tratandoDado(dadosJSON[0].series[1].serie[17][2005]), 0];
@@ -365,8 +376,7 @@ function dadosGrafico(value){
                 dadosListaIPD[3] = [0,tratandoDado(dadosJSON[0].series[1].serie[28][2010])];
                 dadosListaIPD[4] = [0,tratandoDado(dadosJSON[0].series[1].serie[39][2015])];
                 dadosListaIPD[5] = [0,tratandoDado(dadosJSON[0].series[1].serie[48][2020])];
-            }else{
-                printConsole("aqui");
+            }else if(dadosJSON[1].series[1] == null){
                 dadosListaIPD[0] = [tratandoDado(dadosJSON[0].series[1].serie[3][1995]), 0];
                 dadosListaIPD[1] = [tratandoDado(dadosJSON[0].series[1].serie[6][2000]), 0];
                 dadosListaIPD[2] = [tratandoDado(dadosJSON[0].series[1].serie[17][2005]), 0];
@@ -390,15 +400,15 @@ function dadosGrafico(value){
             dadosListaPIB[3] = [ tratandoDado(dadosJSON[2].series[0].serie[28][2010]), tratandoDado(dadosJSON[2].series[1].serie[28][2010])];
             dadosListaPIB[4] = [ tratandoDado(dadosJSON[2].series[0].serie[39][2015]), tratandoDado(dadosJSON[2].series[1].serie[39][2015])];
             dadosListaPIB[5] = [ tratandoDado(dadosJSON[2].series[0].serie[48][2020]), tratandoDado(dadosJSON[2].series[1].serie[48][2020])];
-        }else if(dadosJSON[3].series.length == 1){
-            if(dadosJSON[1].series[0] == null){
+        }else if(dadosJSON[2].series.length == 1){
+            if(dadosJSON[2].series[0] == null){
                 dadosListaPIB[0] = [0,tratandoDado(dadosJSON[0].series[1].serie[3][1995])];
                 dadosListaPIB[1] = [0,tratandoDado(dadosJSON[0].series[1].serie[6][2000])];
                 dadosListaPIB[2] = [0,tratandoDado(dadosJSON[0].series[1].serie[17][2005])];
                 dadosListaPIB[3] = [0,tratandoDado(dadosJSON[0].series[1].serie[28][2010])];
                 dadosListaPIB[4] = [0,tratandoDado(dadosJSON[0].series[1].serie[39][2015])];
                 dadosListaPIB[5] = [0,tratandoDado(dadosJSON[0].series[1].serie[48][2020])];
-            }else{
+            }else if(dadosJSON[2].series[1] == null){
                 dadosListaPIB[0] = [tratandoDado(dadosJSON[0].series[1].serie[3][1995]), 0];
                 dadosListaPIB[1] = [tratandoDado(dadosJSON[0].series[1].serie[6][2000]), 0];
                 dadosListaPIB[2] = [tratandoDado(dadosJSON[0].series[1].serie[17][2005]), 0];
@@ -415,8 +425,16 @@ function dadosGrafico(value){
             dadosListaPIB[5] = [0, 0];
         }
     }
-}
 
+    printConsole('tratou todos os dados')
+    criarGrafico()
+}
+function criarGrafico(){
+    printConsole('vai criar o gráfico')
+    google.charts.setOnLoadCallback(drawVisualizationIPD);
+    google.charts.setOnLoadCallback(drawVisualizationGastosComEducacao);
+    google.charts.setOnLoadCallback(drawVisualizationPIB);
+}
 function tratandoDado(value){
     
     if(value == null || isNaN(value)){
@@ -433,7 +451,7 @@ function printConsole(value1){
 }
 
 function drawVisualizationGastosComEducacao() {
-    printConsole(controle);
+    printConsole("dentro da chamada " + controle);
     //Some raw data (not necessarily accurate)
     var data;
     if(controle == 1){
@@ -528,8 +546,11 @@ function drawVisualizationPIB(){
         data = google.visualization.arrayToDataTable([
             ['Ano', paisGrafico1, paisGrafico2],
             ["1995", dadosListaPIB[0][0], dadosListaPIB[0][1]],
+
+            
             ["2000", dadosListaPIB[1][0], dadosListaPIB[1][1]],
-            ["2005", dadosListaPIB[2][0], dadosListaPIB[2][1]],
+            ["2005", dadosListaPIB[2][0], dadosListaPIB[
+                2][1]],
             ["2010", dadosListaPIB[3][0], dadosListaPIB[3][1]],
             ["2015", dadosListaPIB[4][0], dadosListaPIB[4][1]],
             ["2020", dadosListaPIB[5][0], dadosListaPIB[5][1]]
